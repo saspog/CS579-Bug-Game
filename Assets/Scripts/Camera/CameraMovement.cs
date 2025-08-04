8eea1a94aa2a151c5dc6575e332193bafa27fcc3
Ogerio <73499953+Ogerio@users.noreply.github.com>
2025-08-04 18:44:42 -0500

    World and tree boundries

--- Assets/Scripts/Camera/CameraMovement.cs
+++ Assets/Scripts/Camera/CameraMovement.cs
@@ -32,5 +32,7 @@ public class CameraMovement : MonoBehaviour
     void OnCollisionEnter(Collision collision)
     {
         Debug.Log("Collided with: " + collision.gameObject.name);
+        GetComponent<Rigidbody>().velocity = Vector3.zero;
+        GetComponent<Rigidbody>().angularVelocity = Vector3.zero;
     }
 }
